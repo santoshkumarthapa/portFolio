19c5f64b73689ffcf451fe935040645353f67e5a
Thapa Kumar <santosh.thapa.kumar@accenture.com>
2023-01-13 12:35:36 +0530

    added url file in app itself so that in mail url file can we use multiple app url with out having conflitcs

--- portFolio/home/views.py
+++ portFolio/home/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def my_intro(request):
+    return HttpResponse('<h1> Welcome to my web page </h1>')
